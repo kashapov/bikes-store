5c87e0de35e4fbf817bea5812d2269548ae8dafa
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-17 15:03:39 +0300

    feat: show/hide bike details

--- components/app.js
+++ components/app.js
@@ -1,23 +1,45 @@
-createBike = (id, brand, model, year, price, image) => ({
+createBike = (id, brand, model, year, price, image, sizes, details) => ({
   id,
   brand,
   model,
   year,
   price,
-  image
+  image,
+  sizes,
+  details
 });
 
 const bikes = [
-  createBike(1, "Rondo", "Ruut ST", 2019, 1000, "assets/images/rondo-ruut.jpg"),
+  createBike(
+    1,
+    "Rondo",
+    "Ruut ST",
+    2019,
+    1000,
+    "assets/images/rondo-ruut.jpg",
+    "L, M, XL",
+    ["frame: steel", "fork: carbon", "speed: 1x12", "brakes: hydraulic"]
+  ),
   createBike(
     2,
     "Lynskey",
     "Lynskey GR270 Titanium",
     2019,
     2000,
-    "assets/images/Lynskey-GR270-Titanium.jpg"
+    "assets/images/Lynskey-GR270-Titanium.jpg",
+    "S, L, XL",
+    ["frame: titan", "fork: carbon", "speed: 2x10", "brakes: hydraulic"]
   ),
-  createBike(3, "Basso", " Palta", 2019, 1500, "assets/images/basso-palta.jpg")
+  createBike(
+    3,
+    "Basso",
+    " Palta",
+    2019,
+    1500,
+    "assets/images/basso-palta.jpg",
+    "M",
+    ["frame: carbon", "fork: carbon", "speed: 1x11", "brakes: hydraulic"]
+  )
 ];
 
 new Vue({
@@ -25,13 +47,19 @@ new Vue({
   data: {
     bikes,
     bike: bikes[0],
-    selectedBike: 0
+    selectedBike: 0,
+    isBikeDetails: false
   },
   methods: {
     selectBike: function(index) {
       console.log(`select bike ${index}`);
       this.bike = bikes[index];
       this.selectedBike = index;
+    }
+  },
+  computed: {
+    detailsBtnText() {
+      return this.isBikeDetails ? "Hide details" : "Show details";
     }
   }
 });
